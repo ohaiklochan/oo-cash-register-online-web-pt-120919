18e9a1d99c314c93ef760fd7e85fd44672c8681b
Chloe Foronda <caforonda103@yahoo.com>
2020-01-25 14:53:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
-
+class CashRegister
+  
+  attr_accessor :total, :discount, :item, :last_transaction
+  
+  def intialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+  def add_item(name, price, amount = 1)
+    self.total = price * amount
+    @items << name
+  end
+  
+end
+    
+  
+  
+  
